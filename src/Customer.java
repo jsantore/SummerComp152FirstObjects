b4e4bcfea2253aa551c3b305ed9ff1304b4938dd
johns <jsantore@bridgew.edu>
2025-06-02 13:40:19 -0400

    customer now has accounts

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,26 @@
+import java.util.ArrayList;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private String middleName;
+    private ArrayList<BankAccount> accounts;
+
+    public Customer(){
+        accounts = new ArrayList<>();
+    }
 
     public void setName(String fName,
                         String lName, String mName){
         firstName = fName;
         lastName = lName;
         middleName = mName;
+    }
+
+
+    public void openAccount(double initialBalance){
+        var newAccount = new BankAccount(initialBalance, 0.01f);
+        accounts.add(newAccount);
     }
 
     public String getName(){
--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,26 @@
 
 
 void main(){
-    var profsAccount = new BankAccount(1000, 0.05f);
-    var yourAccount = new BankAccount(500, 0.02f);
-    yourAccount.deposit(500);
-    profsAccount.deposit(1000);
-    profsAccount.addInterest();
-    yourAccount.addInterest();
-    System.out.println("Profs account balance: " + profsAccount.checkBalance()+
-            " Prof's account number "+ profsAccount.getAccountNumber());
-    System.out.println("Your account balance: " + yourAccount.checkBalance()+
-            " Your account number "+ yourAccount.getAccountNumber());
-    profsAccount.withdraw(100);
-    System.out.println("Profs account balance: " + profsAccount.checkBalance());
-    System.out.println("Your account balance: " + yourAccount.checkBalance());
-    if (profsAccount.withdraw(1000)) {
-        System.out.println("Profs account balance: " + profsAccount.checkBalance());
-    } else {
-        System.out.println("Insufficient funds");
-    }
+
+    var customer = new Customer();
+    customer.setName("Prof", null, "Santore");
+    customer.openAccount(1000);
+//    var profsAccount = new BankAccount(1000, 0.05f);
+//    var yourAccount = new BankAccount(500, 0.02f);
+//    yourAccount.deposit(500);
+//    profsAccount.deposit(1000);
+//    profsAccount.addInterest();
+//    yourAccount.addInterest();
+//    System.out.println("Profs account balance: " + profsAccount.checkBalance()+
+//            " Prof's account number "+ profsAccount.getAccountNumber());
+//    System.out.println("Your account balance: " + yourAccount.checkBalance()+
+//            " Your account number "+ yourAccount.getAccountNumber());
+//    profsAccount.withdraw(100);
+//    System.out.println("Profs account balance: " + profsAccount.checkBalance());
+//    System.out.println("Your account balance: " + yourAccount.checkBalance());
+//    if (profsAccount.withdraw(1000)) {
+//        System.out.println("Profs account balance: " + profsAccount.checkBalance());
+//    } else {
+//        System.out.println("Insufficient funds");
+//    }
 }
