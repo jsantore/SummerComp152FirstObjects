c1111e1954d01bd3a9bbf0c618fcdf9b0d9c4568
johns <jsantore@bridgew.edu>
2025-06-02 11:16:14 -0400

    Create simplest Bank Account Update Main to java 21 style, not using bank accounts yet Added initial BankAccount Design doc

---
+++ src/BankAccount.java
@@ -0,0 +1,27 @@
+public class BankAccount {
+    private double balance;
+    private int accountNumber;
+    private float interestRate;
+
+    public double checkBalance(){
+        return balance;
+    }
+
+    public double addInterest(){
+        balance += balance * interestRate;
+        return balance;
+    }
+
+    public void deposit(double amount){
+        balance = balance + amount;
+    }
+
+    public boolean withdraw(double amount){
+        if(balance >= amount){
+            balance = balance - amount;
+            return true;
+        }
+        return false;
+    }
+
+}
